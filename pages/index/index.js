6df1c31197df1c2b10d078974d7aff6f3b49c8ec
fengshengqi777 <1075121391@qq.com>
2019-07-05 12:41:14 +0800

    提交文件

---
+++ pages/index/index.js
@@ -0,0 +1,103 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    showLogin: false, //显示授权框
+    session_key: null,
+    openid: null
+  },
+  // 检测是否登录
+  checkSession: function () {
+    var that = this;
+    wx.checkSession({
+      success(res) {
+        wx.getStorage({
+          key: 'phone',
+          success(res) {
+            if (res.data) {
+              that.setData({
+                phoneNumber: res.data
+              })
+            } else {
+              that.setData({
+                showLogin: true
+              })
+            }
+          }
+        });
+      },
+      fail(res) {
+        that.login();
+        that.setData({
+          showLogin: true
+        })
+
+      }
+    })
+  },
+  // 获取手机号
+  getPhoneNumber: function (e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.iv)
+    console.log(e.detail.encryptedData)
+    if (e.detail.errMsg == 'getPhoneNumber:fail user deny') {
+      wx.showModal({
+        title: '提示',
+        showCancel: false,
+        content: '未授权',
+        success: function (res) { }
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        showCancel: false,
+        content: '同意授权',
+        success: function (res) { }
+      })
+    }
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
